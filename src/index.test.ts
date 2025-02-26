fe34011cabb7918bf05290e85927f6f1a60b8cb6
Inbarasan <inbarasanraja@gmail.com>
2025-02-26 14:47:31 +0530

    chore: fix test sentence

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,7 +2,7 @@ import { sum } from "./index";
 import {describe, it, expect} from "@jest/globals";
 
 describe('Sum function', () =>{
-    it('should be returned zero when empty string is passed ', () =>{
+    it('should return zero when empty string is passed ', () =>{
         expect(sum("")).toBe(0);
     })
 })
